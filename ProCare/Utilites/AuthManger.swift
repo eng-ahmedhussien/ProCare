8019f3524583e790dff561ca0d3762c835292c07
ahmed hussien <133219145+ahmedhusseinDBS@users.noreply.github.com>
2025-06-30 14:43:35 +0300

    refactor: Clean up AuthManager by removing unused code and enhancing profile data loading

--- ProCare/Utilites/AuthManger.swift
+++ ProCare/Utilites/AuthManger.swift
@@ -9,63 +9,20 @@ import Foundation
 import Security
 import SwiftUI
 
-//final class AuthManager: ObservableObject {
-//    static let shared = AuthManager()
-//
-//    @Published private(set) var token: String?
-//    @Published private(set) var userData: UserDataLogin?
-//
-//    private init() {
-//        self.token = UserDefaults.standard.string(forKey: "auth_token")
-//
-//        if let data = UserDefaults.standard.data(forKey: "user_data"),
-//           let decoded = try? JSONDecoder().decode(UserDataLogin.self, from: data) {
-//            self.userData = decoded
-//        }
-//    }
-//
-//    var isLoggedIn: Bool {
-//        return token != nil
-//    }
-//
-//    func saveToken(_ token: String) {
-//        self.token = token
-//        UserDefaults.standard.set(token, forKey: "auth_token")
-//    }
-//
-//    func saveUserData(_ userData: UserDataLogin) {
-//        self.userData = userData
-//        if let encoded = try? JSONEncoder().encode(userData) {
-//            UserDefaults.standard.set(encoded, forKey: "user_data")
-//        }
-//    }
-//
-//    func getToken() -> String? {
-//        return token
-//    }
-//
-//    func getUserData() -> UserDataLogin? {
-//        return userData
-//    }
-//
-//    func logout() {
-//        token = nil
-//        userData = nil
-//        UserDefaults.standard.removeObject(forKey: "auth_token")
-//        UserDefaults.standard.removeObject(forKey: "user_data")
-//    }
-//}
 @MainActor
 class AuthManager: ObservableObject {
     @Published var isLoggedIn: Bool = false
     @Published var userDataLogin: UserDataLogin? = nil
+    @Published var profileData: Profile? = nil
 
-    private let defaults = AppUserDefaults.shared
+   // private let defaults = AppUserDefaults.shared
     private let keychainHelper = KeychainHelper.shared
 
     init() {
         loadUserData()
+        loadProfileData()
     }
+    
     
     private func loadUserData() {
         if let savedUserData = keychainHelper.getData(UserDataLogin.self, forKey: .userData),
@@ -75,14 +32,11 @@ class AuthManager: ObservableObject {
         } else {
             self.isLoggedIn = false
         }
-         
-        
-//        if let savedUserData = defaults.getCodable(UserDataLogin.self, forKey: .userData) {
-//            self.userDataLogin = savedUserData
-//            self.isLoggedIn = (savedUserData.token?.isEmpty == false)
-//        } else {
-//            self.isLoggedIn = false
-//        }
+    }
+    
+    private func loadProfileData() {
+        guard let savedProfileData = keychainHelper.getData( Profile.self,forKey: .profileData) else {return}
+        self.profileData = savedProfileData
     }
     
     func login(userDataLogin: UserDataLogin) {
@@ -93,11 +47,11 @@ class AuthManager: ObservableObject {
     }
     
     func logout() {
-        defaults.remove(forKey: .userData)
-        defaults.remove(forKey: .profileData)
         keychainHelper.delete(forKey: .authToken)
         keychainHelper.delete(forKey: .userData)
+        keychainHelper.delete(forKey: .profileData)
         self.userDataLogin = nil
+        self.profileData = nil
         self.isLoggedIn = false
     }
 }
