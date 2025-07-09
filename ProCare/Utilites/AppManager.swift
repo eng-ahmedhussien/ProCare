81b30971c7ee00b895f35a1ef305109d67d541cb
ahmed hussien <133219145+ahmedhusseinDBS@users.noreply.github.com>
2025-07-09 10:17:45 +0300

    feat: Implement  logout functionality

--- ProCare/Utilites/AppManager.swift
+++ ProCare/Utilites/AppManager.swift
@@ -48,6 +48,24 @@ class AppManager: ObservableObject {
             
         }
     }
+    
+    func logout(completion: @escaping () -> Void) async{
+        let parameters: [String: Any] = [
+            "token": KeychainHelper.shared.get(forKey: .deviceToken) ?? ""
+        ]
+        
+        do {
+            let response = try await apiClient.logout(parameters: parameters)
+            if let data = response.data, data {
+                showToast(response.message ?? "", appearance: .success)
+                completion()
+            }else{
+                showToast(response.message ?? "", appearance: .error)
+            }
+        } catch {
+            showToast(" \(error.localizedDescription)", appearance: .error)
+        }
+    }
 }
 
 struct versionData:Codable{
@@ -58,36 +76,46 @@ struct versionData:Codable{
 enum AppManagerEndPoints: APIEndpoint {
     
     case forceUpdate(parameters: [String:Any])
+    case logout(parameters: [String: Any])
     
     var path: String {
         switch self {
         case .forceUpdate:
             return "/AppVersion/ForceUpdate"
+        case .logout:
+            return "Auth/Logout"
         }
     }
     
     var method: HTTPMethod {
         switch self {
-        case .forceUpdate:
+        case .forceUpdate, .logout:
             return .post
         }
     }
     
     var headers: HTTPHeader? {
-        return .default
+        switch self {
+        case .logout:
+            return .authHeader
+        default:
+            return .default
+        }
     }
     
     var task: Parameters {
         switch self {
         case .forceUpdate(let parameters):
             return .requestParameters(parameters: parameters, encoding: .JSONEncoding())
-       
+        case .logout(let parameters):
+            return .requestParameters(parameters: parameters, encoding: .JSONEncoding())
         }
     }
 }
 
 protocol AppManagerApiClintProtocol {
     func forceUpdate(parameters: [String:Any]) async throws -> APIResponse<versionData>
+    func logout(parameters: [String: Any]) async throws -> APIResponse<Bool>
 
 }
 
@@ -95,5 +123,9 @@ class AppManagerApiClint : ApiClient<AppManagerEndPoints>, AppManagerApiClintPro
     
     func forceUpdate(parameters: [String:Any] ) async throws -> APIResponse<versionData> {
         return try await request(.forceUpdate(parameters: parameters))
+    }
+    
+    func logout(parameters: [String: Any]) async throws -> APIResponse<Bool> {
+        return try await request(.logout(parameters: parameters))
     }
 }
