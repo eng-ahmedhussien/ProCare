beea4b0304528c415302a30ef8a8939ec9774c23
ahmed hussien <133219145+ahmedhusseinDBS@users.noreply.github.com>
2025-07-09 10:19:27 +0300

    refactor: Improve error handling and code structure in NuresProfileVM

--- ProCare/Core/Features/NuresRole/Profile/VM/NuresProfileVM.swift
+++ ProCare/Core/Features/NuresRole/Profile/VM/NuresProfileVM.swift
@@ -1,91 +1,98 @@
-//
-//  NuresProfileVM.swift
-//  ProCare
-//
-//  Created by ahmed hussien on 30/05/2025.
-//
+    //
+    //  NuresProfileVM.swift
+    //  ProCare
+    //
+    //  Created by ahmed hussien on 30/05/2025.
+    //
 
-import SwiftUI
-import Combine
+    import SwiftUI
+    import Combine
 
-@MainActor
-class NuresProfileVM: ObservableObject {
-    
-    // MARK: - Published Properties
-    @Published var nurseProfile: NurseProfile?
-    @Published var isBusy: Bool = false
-    
-    private let apiClient: NurseApiClintProtocol
-    private var cancellables: Set<AnyCancellable> = []
-    
-    init(apiClient: NurseApiClintProtocol = NurseApiClint()) {
-        self.apiClient = apiClient
-    }
-    
-    // MARK: - API Methods
-    func fetchNurseProfile() async {
-        do {
-            let response = try await apiClient.getNurseProfile()
-            if let nurseProfile = response.data {
-                self.nurseProfile = nurseProfile
-            } else {
-                
-            }
-        } catch {
-            showToast("Unexpected error: \(error.localizedDescription)", appearance: .error)
-        }
-    }
-    
-    func updateLocation(lat : Double, lon : Double) async {
+    @MainActor
+    class NuresProfileVM: ObservableObject {
         
-        let parameters: [String: Any] = [
-            "latitude": lat,
-            "longitude": lon
-        ]
+        // MARK: - Published Properties
+        @Published var nurseProfile: NurseProfile?
+        @Published var isBusy: Bool = false
         
-        do {
-            let response = try await apiClient.updateLocation(parameters: parameters)
-            if let _ = response.data {
-                showToast("\(response.message ?? "Success")", appearance: .success)
-            } else {
-                debugPrint("Response received but no user data")
+        private let apiClient: NurseApiClintProtocol
+        private var cancellables: Set<AnyCancellable> = []
+        
+        init(apiClient: NurseApiClintProtocol = NurseApiClint()) {
+            self.apiClient = apiClient
+        }
+        
+        // MARK: - API Methods
+        func fetchNurseProfile(onUnauthorized: @escaping () -> Void = {}) async {
+            do {
+                let response = try await apiClient.getNurseProfile()
+                if let nurseProfile = response.data {
+                    self.nurseProfile = nurseProfile
+                } else {
+                    
+                }
+            } catch {
+                if let apiError = error as? APIResponseError, apiError.status == 401 {
+                    debugPrint("RequestsVM: Unauthorized error detected")
+                    onUnauthorized()
+                } else {
+                    debugPrint(" NuresProfileVM -> fetchNurseProfile\(error.localizedDescription)")
+                    showToast("\(error.localizedDescription)", appearance: .error)
+                }
             }
-        } catch {
-            showToast("Unexpected error: \(error.localizedDescription)", appearance: .error)
         }
-    }
-    
-    func changeStatus(isBusy: Bool, onSuccess: ((Bool) -> Void)? = nil) async {
-        do {
-            let response = try await apiClient.changeStatus(isBusy: isBusy)
-            handleApiResponse(response, onSuccess: onSuccess)
-        } catch {
-            showToast("Unexpected error: \(error.localizedDescription)", appearance: .error)
-        }
-    }
-    
-    private func handleApiResponse<T: Codable>(_ response: APIResponse<T>, onSuccess: ((Bool) -> Void)? = nil) {
-            switch response.status {
-            case .Success:
-                if let responseData = response.data {
-                    switch responseData as! Bool {
-                    case true:
-                        showToast("\(response.message ?? "")", appearance: .success)
-                        onSuccess?(true)
-                    case false:
-                        showToast(" \(response.message ?? "")", appearance: .error)
-                        onSuccess?(false)
-                    }
+        
+        func updateLocation(lat : Double, lon : Double) async {
+            
+            let parameters: [String: Any] = [
+                "latitude": lat,
+                "longitude": lon
+            ]
+            
+            do {
+                let response = try await apiClient.updateLocation(parameters: parameters)
+                if let _ = response.data {
+                    showToast("\(response.message ?? "Success")", appearance: .success)
                 } else {
                     debugPrint("Response received but no user data")
                 }
-            case .Error, .AuthFailure, .Conflict:
-                showToast(
-                    response.message ?? "network error",
-                    appearance: .error
-                )
-            case .none:
-                break
+            } catch {
+                showToast("Unexpected error: \(error.localizedDescription)", appearance: .error)
             }
         }
-}
+        
+        func changeStatus(isBusy: Bool, onSuccess: ((Bool) -> Void)? = nil) async {
+            do {
+                let response = try await apiClient.changeStatus(isBusy: isBusy)
+                handleApiResponse(response, onSuccess: onSuccess)
+            } catch {
+                showToast("Unexpected error: \(error.localizedDescription)", appearance: .error)
+            }
+        }
+        
+        private func handleApiResponse<T: Codable>(_ response: APIResponse<T>, onSuccess: ((Bool) -> Void)? = nil) {
+                switch response.status {
+                case .Success:
+                    if let responseData = response.data {
+                        switch responseData as! Bool {
+                        case true:
+                            showToast("\(response.message ?? "")", appearance: .success)
+                            onSuccess?(true)
+                        case false:
+                            showToast(" \(response.message ?? "")", appearance: .error)
+                            onSuccess?(false)
+                        }
+                    } else {
+                        debugPrint("Response received but no user data")
+                    }
+                case .Error, .AuthFailure, .Conflict:
+                    showToast(
+                        response.message ?? "network error",
+                        appearance: .error
+                    )
+                case .none:
+                    break
+                }
+            }
+        
+    }
